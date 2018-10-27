9c045c4725c2a3b865f26b008d08449afdbf38b1
Bradley Modern <modern.bradley@gmail.com>
2018-10-27 16:19:41 +0100

    Basic scene, player movement and looking with the camera

---
+++ Warden/Assets/Scripts/playerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float moveSpeed = 7.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        float zAxisSpeed = (Input.GetAxis("Vertical") * moveSpeed) * Time.deltaTime;
+        float xAxisSpeed = (Input.GetAxis("Horizontal") * moveSpeed) * Time.deltaTime;
+
+        transform.Translate(xAxisSpeed, 0, zAxisSpeed);
+
+        if(Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+}
