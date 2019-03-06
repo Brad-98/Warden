d47b45d48d64d16821a6250c572bc2fc840e80c7
Bradley Modern <modern.bradley@gmail.com>
2019-03-06 23:21:23 +0000

    Basic Main Menu, Basic Choose Level Menu
    Create a basic main menu
    
    Create a menu to choose what level you want to play

---
+++ Warden/Assets/Scripts/buttonController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class buttonController : MonoBehaviour
+{
+    public void mainMenuToChooseLevel()
+    {  
+        SceneManager.LoadScene("chooseLevel", 0);
+    }
+
+    public void knightV1()
+    {
+        SceneManager.LoadScene("Knight_V1", 0);
+    }
+
+    public void knightV2()
+    {
+        SceneManager.LoadScene("Knight_V2", 0);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
