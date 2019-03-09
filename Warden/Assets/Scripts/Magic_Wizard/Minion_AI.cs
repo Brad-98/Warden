24e23738d8dc32f774e3a9ad322246a9632d4210
Bradley Modern <modern.bradley@gmail.com>
2019-03-09 20:32:37 +0000

    Minions Updated
    Wizard_V3 minions now follow and attack the player and can die

--- Warden/Assets/Scripts/Magic_Wizard/Minion_AI.cs
+++ Warden/Assets/Scripts/Magic_Wizard/Minion_AI.cs
@@ -7,18 +7,38 @@ public class Minion_AI : MonoBehaviour
     public float enemyMoveSpeed = 2;
     private float currentEnemyMoveSpeed;
 
+    public int maxEnemyHealth = 1;
+    private int currentEnemyHealth;
+
     Transform target;
     
     void Start()
     {
         currentEnemyMoveSpeed = enemyMoveSpeed;
 
+        currentEnemyHealth = maxEnemyHealth;
+
         target = GameObject.Find("Player/enemyTarget").transform;
     }
 
     void Update()
     {
+        if(currentEnemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
         transform.LookAt(target.position);
         transform.position += transform.forward * currentEnemyMoveSpeed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Axe") //GET THE TIMERS RIGHT FOR ATTACKING ENEMY
+        {
+
+            //StartCoroutine(Wait());
+            currentEnemyHealth -= GameObject.Find("Player").GetComponent<playerAttack>().playerDamageValue;
+            //Debug.Log(currentEnemyHealth);
+        }
     }
 }
